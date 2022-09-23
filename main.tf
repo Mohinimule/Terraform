dfd9225d857864980d121b28c7574afdc2974301
Mohinimule <65327108+Mohinimule@users.noreply.github.com>
2022-09-23 11:58:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "myawsserver" {
   tags = {
     Name = lower (var.name)
     env = upper (var.env)
-    owner = upper ("Techlanders")
+    owner = upper ("Mohini")
     dept = "lab"
     client = "vodafone"
   }
