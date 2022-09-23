6c3738038478d33b854b90e278b7f7cea24543db
Mohinimule <65327108+Mohinimule@users.noreply.github.com>
2022-09-23 11:54:49 +0530

    Delete modules directory

--- modules/S3_Demo/main.tf
+++
@@ -1,8 +0,0 @@
-  resource "aws_s3_bucket" "S3_demo" {
-  bucket = "${var.bucket}"
-  acl    = "private"
-
-  tags {
-    Name = "Mybucket"
-  }
-}
--- modules/S3_Demo/variable.tf
+++
@@ -1,3 +0,0 @@
-variable "bucket" {
-  default = "My_first_bucket"
-}
